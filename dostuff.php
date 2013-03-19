270bda1331357c26ca4b06276023a3200ece6afd
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-19 17:34:48 +0100

    Attempting to generate wiki pages from travis.

--- dostuff.php
+++ dostuff.php
@@ -1,9 +1,9 @@
 <?php
 echo "Attempting to generate wiki page."
-`git clone git@github.com:akimsko/box.wiki.git`;
+system('git clone git@github.com:akimsko/box.wiki.git');
 chdir('box.wiki.git');
 file_put_contents('Lol.md', 'Test');
-`git add Lol.md`;
-`git commit -m "Lol.md"`;
-`git push`; 
+system('git add Lol.md');
+system('git commit -m "Modified Lol.md"');
+system('git push'); 
 
