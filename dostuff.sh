813c4a2025ef059730c054df08415d2748ecab87
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-19 18:03:38 +0100

    Attempting to generate wiki pages from travis.

--- dostuff.sh
+++ dostuff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod a+rwx git_ssh_options
-export GIT_SSH=`pwd`"/git_ssh_options"
+export GIT_SSH=`pwd`"/git_ssh_command"
 git config --global user.name "Travis"
 git config --global user.email "noreply@travis-ci.org"
 git clone git@github.com:akimsko/box.wiki.git
