08bfaa1efecb20a6710ae3a9b7456eb87d732d4f
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-19 18:45:37 +0100

    Trying stuff.

--- dostuff.sh
+++ dostuff.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 chmod a+rwx git_ssh_command
 export GIT_SSH=`pwd`"/git_ssh_command"
+cd ~
+ls -ah .ssh
+cat .ssh/id_rsa.pub
 git config --global user.name "Travis"
 git config --global user.email "noreply@travis-ci.org"
 git clone git@github.com:akimsko/box.wiki.git
