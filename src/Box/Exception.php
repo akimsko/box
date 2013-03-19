609d63567f954b437104a9969e20df28d2f13ee1
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-19 11:17:19 +0100

    Added framework exception class.

--- src/Box/Exception.php
+++ src/Box/Exception.php
@@ -3,14 +3,14 @@
 /*
  * This file is part of the Box project.
  * @link https://github.com/akimsko/box
- * 
+ *
  * @copyright Copyright 2013 Bo Thinggaard & Jens Riisom Schultz
  * @license   http://www.apache.org/licenses/LICENSE-2.0
  */
 namespace Box;
 /**
- * StoreException
+ * An exception which occured inside the Box framework.
  *
- * @author Bo Thinggaard <bo@unpossiblesystems.dk>
+ * @author Jens Riisom Schultz <jens@unpossiblesystems.dk>
  */
-class StoreException extends Exception { }
+class Exception extends \Exception { }
