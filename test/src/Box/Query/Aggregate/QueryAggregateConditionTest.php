319577b1be195d94de972aaf7ab2fd17c6cdfe31
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-19 11:03:32 +0100

    Added QueryAggregateOperation tests.

--- test/src/Box/Query/Aggregate/QueryAggregateConditionTest.php
+++ test/src/Box/Query/Aggregate/QueryAggregateConditionTest.php
@@ -15,7 +15,7 @@ class QueryAggregateConditionTest extends \PHPUnit_Framework_TestCase {
 	 * This method is called before a test is executed.
 	 */
 	protected function setUp() {
-		$this->object = new QueryAggregateCondition;
+		$this->object = new QueryAggregateCondition();
 	}
 
 	/**
