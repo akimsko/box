c564c379b819ee1c4133d5e166b52147a2be8931
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-21 13:11:50 +0100

    Fixed a broken namespace declaration.

--- src/Box/Store/Unified.php
+++ src/Box/Store/Unified.php
@@ -7,7 +7,7 @@
  * @copyright Copyright 2013 Bo Thinggaard & Jens Riisom Schultz
  * @license   http://www.apache.org/licenses/LICENSE-2.0
  */
-namespace \Box;
+namespace Box;
 /**
  * StoreUnified
  *
