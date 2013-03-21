bb838a012995628c4e9ea010726a1a2261737098
Jens Riisom Schultz <jens@unpossiblesystems.dk>
2013-03-21 11:09:07 +0100

    Trying to cd to a folder that exists.

--- dostuff.sh
+++ dostuff.sh
@@ -6,7 +6,7 @@ cd ~
 git config --global user.name "Travis"
 git config --global user.email "noreply@travis-ci.org"
 git clone git@github.com:akimsko/box.wiki.git
-cd box.wiki.git
+cd box.wiki
 echo 'Lol' > Lol.md
 git add Lol.md
 git commit -m "Modified Lol.md" && git push
